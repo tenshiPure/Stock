ac67d31c08bebf92d7253048514af5ccce2db195
tenshiPure <user.ryo@gmail.com>
2015-08-30 17:23:46 +0900

    markup stock list

--- Handler/Mock.hs
+++ Handler/Mock.hs
@@ -8,8 +8,10 @@ import Database.Persist.Sql(toSqlKey)
 getInitR :: Handler Html
 getInitR = do
     _ <- runDB $ deleteWhere ([] :: [Filter Stock])
-    _ <- runDB $ insert $ Stock "ノバレーゼ" 2128 "結婚式場の運営" "http://www.novarese.co.jp/corp/" "高め"
-    _ <- runDB $ insert $ Stock "元気寿司" 9828 "回転寿司店" "http://www.genkisushi.co.jp/" "良さげ"
+    _ <- runDB $ insert $ Stock "ノバレーゼ" 2128 "結婚式場の運営" "http://www.novarese.co.jp/corp/" "高め、28ページ"
+    _ <- runDB $ insert $ Stock "元気寿司" 9828 "回転寿司店" "http://www.genkisushi.co.jp/" "良さげ、148ページ"
+    _ <- runDB $ insert $ Stock "ビックカメラ" 3048 "家電 首都圏が中心" "http://www.biccamera.co.jp/bicgroup/index.html" "あはは、88ページ"
+    _ <- runDB $ insert $ Stock "シダックス" 4837 "給食、レストラン、カラオケ" "http://www.shidax.co.jp/" "うはは、108ページ"
 
     _ <- runDB $ deleteWhere ([] :: [Filter Present])
     _ <- runDB $ insert $ Present 100 "レストラン優待券（30%割引）x 1" (toSqlKey 1 :: StockId)
